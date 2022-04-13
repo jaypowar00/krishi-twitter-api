e5792b53440ba65dd8f8df1718a4a4ff35be0e63
jaypowar00 <jaypowar00@gmail.com>
2022-04-13 20:35:51 +0530

    added logs for debug

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,6 @@ from app.weather.routs import weather
 from app.posts.models import post_db, Posts
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from dotenv import load_dotenv
-load_dotenv()
 
 
 app = Flask(__name__)
@@ -15,12 +13,16 @@ db = SQLAlchemy(app)
 app.register_blueprint(posts)
 app.register_blueprint(weather)
 
+print('[+] db uri:', end=' ')
+print(os.getenv('DATABASE_URI'))
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app, expose_headers=['Access-Control-Allow-Origin'], supports_credentials=True)
 
 print('[+] in __init__ file')
 
+post_db.init_app(app)
+post_db.create_all()
 def create_app():
     print('2')
     post_db.init_app(app)
