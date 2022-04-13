75f3f9895a3d2dc70d36a14e4a7d024022dd6c7f
jaypowar00 <jaypowar00@gmail.com>
2022-04-13 19:24:28 +0530

    added cors config in init

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from app.posts.routs import posts
 from app.weather.routs import weather
 from app.posts.models import post_db, Posts
+from flask_cors import CORS
 app = Flask(__name__)
 
 app.register_blueprint(posts)
@@ -10,6 +11,8 @@ app.register_blueprint(weather)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+CORS(app, expose_headers=['Access-Control-Allow-Origin'], supports_credentials=True)
 
 post_db.init_app(app)
 post_db.app = app
